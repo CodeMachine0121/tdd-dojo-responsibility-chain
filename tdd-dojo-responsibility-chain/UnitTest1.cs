ee875744a732f89bd21e34b1608abeb811ac1361
james.hsueh77 <james.hsueh@titansoft.com.sg>
2024-02-04 02:07:09 +0800

    first commit

---
+++ tdd-dojo-responsibility-chain/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace tdd_dojo_responsibility_chain;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    { }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
