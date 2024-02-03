9840387fd31c4acb9428512ae1a82644ce9ef7a6
james.hsueh77 <james.hsueh@titansoft.com.sg>
2024-02-04 03:17:33 +0800

    is able to set switch type

--- tdd-dojo-responsibility-chain/KeyBoardAssembleServiceTests.cs
+++ tdd-dojo-responsibility-chain/KeyBoardAssembleServiceTests.cs
@@ -16,18 +16,26 @@ public class KeyBoardAssembleServiceTests
     [Test]
     public void should_get_circuit_board()
     {
-        _keyboard = _keyBoardAssembleService.Generate("Logic");
+        _keyboard = _keyBoardAssembleService.Generate("Logic", "any_switch");
         Assert.AreEqual("Logic", _keyboard.CircuitBoard);
+    }
+
+    [Test]
+    public void should_set_switch()
+    {
+        _keyboard = _keyBoardAssembleService.Generate("Logic", "red");
+        Assert.AreEqual("red", _keyboard.SwitchType);
     }
 
 }
 
 public class KeyBoardAssembleService
 {
-    public Keyboard Generate(string brandName)
+    public Keyboard Generate(string brandName, string switchType)
     {
         var keyboard = new Keyboard();
         keyboard.GetCircuitBoard(brandName);
+        keyboard.SetSwitchType(switchType);
         return keyboard;
     }
 }
@@ -35,6 +43,7 @@ public class KeyBoardAssembleService
 public class Keyboard
 {
     public string CircuitBoard { get; set; }
+    public string SwitchType { get; set; }
 
     public bool IsIncludeCircuitBoard()
     {
@@ -44,5 +53,10 @@ public class Keyboard
     public void GetCircuitBoard(string brandName)
     {
         CircuitBoard = brandName;
+    }
+
+    public void SetSwitchType(string switchType)
+    {
+        SwitchType = switchType;
     }
 }
